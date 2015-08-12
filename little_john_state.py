81324d5cd6f9b5845ffed36386bb77e03cf9d0a9
Paul Chun <me@paulchun.com>
2015-08-12 15:40:13 -0700

    Tidy up and add comments

--- little_john_state.py
+++ little_john_state.py
@@ -6,22 +6,31 @@ class LittleJohnStateError(Exception):
     pass
 
 
-class LittleJohnDecision(Enum):
-    STAND = Enum("LittleJohnDecisionStand",
-                 "stay",
-                 "buy",
-                 "sell")
+class LittleJohnDecision(object):
+    """Little John Decision class
+
+    Attributes:
+        type: (LittleJohnDecision.STAND) The type of decision.
+        symbol: (string) Symbol of the instrument.
+        bid_price: (float) Bid Price of the decision.
+        quantity: (int) Quantity of the decision.
+    """
+
+    TYPE = Enum("LittleJohnDecisionStand",
+                "stay",
+                "buy",
+                "sell")
 
     def __init__(self):
-        self._stand = LittleJohnDecision.STAND.stay
+        self._type = LittleJohnDecision.TYPE.stay
         self._symbol = None
         self._bid_price = None
         self._quantity = None
         self._acted_on = False
 
     @property
-    def stand(self):
-        return self._stand.name
+    def type(self):
+        return self._type.name
 
     @property
     def symbol(self):
@@ -36,34 +45,51 @@ class LittleJohnDecision(Enum):
         return self._quantity
 
     def IsActed(self):
+        """Returns whether decision has been acted on."""
         return self._acted_on
 
     def SetActed(self):
+        """Sets that decision has been acted on."""
         logging.info(
-            "Decision acted on. Details: type: %s, symbol: %s, bid price: %s, quantity: %d",
-            self.stand, self.symbol, self.bid_price, self.quantity)
+            "Decision acted on. Details: type: %s, symbol: %s, bid price: %f, quantity: %d",
+            self.type, self.symbol, self.bid_price, self.quantity)
         self._acted_on = True
 
     @staticmethod
     def Stay():
+        """Creator function that creates a stay decision."""
         decision = LittleJohnDecision()
         decision._SetStayDecision()
         return decision
 
     @staticmethod
     def Buy(symbol, bid_price, quantity):
+        """Creator function that creates a buy decision.
+
+        Args:
+            symbol: (string) Symbol of the instrument.
+            bid_price: (float) Bid Price of the decision.
+            quantity: (int) Quantity of the decision.
+        """
         decision = LittleJohnDecision()
         decision._SetBuyDecision(symbol, bid_price, quantity)
         return decision
 
     @staticmethod
     def Sell(symbol, bid_price, quantity):
+        """Creator function that creates a sell decision.
+
+        Args:
+            symbol: (string) Symbol of the instrument.
+            bid_price: (float) Bid Price of the decision.
+            quantity: (int) Quantity of the decision.
+        """
         decision = LittleJohnDecision()
         decision._SetSellDecision(symbol, bid_price, quantity)
         return decision
 
-    def _SetDecision(self, stand, symbol=None, bid_price=None, quantity=None):
-        self._stand = stand
+    def _SetDecision(self, decision_type, symbol=None, bid_price=None, quantity=None):
+        self._type = decision_type
         self._symbol = symbol
         self._bid_price = bid_price
         self._quantity = quantity
